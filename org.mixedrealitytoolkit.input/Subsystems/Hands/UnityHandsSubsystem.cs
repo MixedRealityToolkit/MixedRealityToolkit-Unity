3cc1fa08476007678859f113f9ed28322aa3afef
Kurtis <quic_keveleig@quicinc.com>
2023-04-13 15:26:36 -0700

    Create UnityHandsSubsystem

---
+++ org.mixedrealitytoolkit.input/Subsystems/Hands/UnityHandsSubsystem.cs
@@ -0,0 +1,256 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using Microsoft.MixedReality.Toolkit.Subsystems;
+using System.Collections.Generic;
+using Unity.Profiling;
+using UnityEngine;
+using UnityEngine.Scripting;
+using UnityEngine.XR;
+using UnityEngine.XR.Hands;
+
+namespace Microsoft.MixedReality.Toolkit.Input
+{
+    [Preserve]
+    [MRTKSubsystem(
+        Name = "com.qualcomm.mixedreality.unityhands",
+        DisplayName = "Subsystem for Unity Hands API",
+        Author = "Qualcomm",
+        ProviderType = typeof(HandsProvider<UnityHandContainer>),
+        SubsystemTypeOverride = typeof(UnityHandsSubsystem),
+        ConfigType = typeof(BaseSubsystemConfig))]
+    public class UnityHandsSubsystem : HandsSubsystem
+    {
+        [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.SubsystemRegistration)]
+        private static void Register()
+        {
+            // Fetch subsystem metadata from the attribute.
+            var cinfo = XRSubsystemHelpers.ConstructCinfo<UnityHandsSubsystem, HandsSubsystemCinfo>();
+
+            // Populate remaining cinfo field.
+            cinfo.IsPhysicalData = true;
+
+            if (!Register(cinfo))
+            {
+                Debug.LogError($"Failed to register the {cinfo.Name} subsystem.");
+            }
+        }
+
+        private class UnityHandContainer : HandDataContainer
+        {
+            private XRHand? hand;
+            private XRHandSubsystem xrHandSubsystem;
+
+            public UnityHandContainer(XRNode handNode) : base(handNode)
+            {
+                hand = GetTrackedHand();
+            }
+
+            private static readonly ProfilerMarker TryGetEntireHandPerfMarker =
+                new ProfilerMarker("[MRTK] UnityHandContainer.TryGetEntireHand");
+
+            /// <inheritdoc/>
+            public override bool TryGetEntireHand(out IReadOnlyList<HandJointPose> result)
+            {
+                using (TryGetEntireHandPerfMarker.Auto())
+                {
+                    if (!AlreadyFullQueried)
+                    {
+                        TryCalculateEntireHand();
+                    }
+
+                    result = handJoints;
+                    return FullQueryValid;
+                }
+            }
+
+            private static readonly ProfilerMarker TryGetJointPerfMarker =
+                new ProfilerMarker("[MRTK] UnityHandContainer.TryGetJoint");
+
+            /// <inheritdoc/>
+            public override bool TryGetJoint(TrackedHandJoint joint, out HandJointPose pose)
+            {
+                using (TryGetJointPerfMarker.Auto())
+                {
+                    bool thisQueryValid = false;
+                    int index = HandsUtils.ConvertToIndex(joint);
+
+                    // If we happened to have already queried the entire
+                    // hand data this frame, we don't need to re-query for
+                    // just the joint. If we haven't, we do still need to
+                    // query for the single joint.
+                    if (!AlreadyFullQueried)
+                    {
+                        hand = GetTrackedHand();
+
+                        // If the tracked hand is null, we obviously have no data,
+                        // and return immediately.
+                        if (!hand.HasValue)
+                        {
+                            pose = handJoints[index];
+                            return false;
+                        }
+
+                        // Joints are relative to the camera floor offset object.
+                        Transform origin = PlayspaceUtilities.XROrigin.CameraFloorOffsetObject.transform;
+                        if (origin == null)
+                        {
+                            pose = handJoints[index];
+                            return false;
+                        }
+
+                        thisQueryValid |= TryUpdateJoint(index, hand.Value.GetJoint(TrackedHandJointIndexToXRHandJointID[index]), origin);
+                    }
+                    else
+                    {
+                        // If we've already run a full-hand query, this single joint query
+                        // is just as valid as the full query.
+                        thisQueryValid = FullQueryValid;
+                    }
+
+                    pose = handJoints[index];
+                    return thisQueryValid;
+                }
+            }
+
+            private static readonly ProfilerMarker GetTrackedHandPerfMarker =
+                new ProfilerMarker("[MRTK] UnityHandContainer.GetTrackedHand");
+
+            /// <summary>
+            /// Obtains a reference to the actual Hand object representing the tracked hand
+            /// functionality present on handNode. Returns null if no Hand reference available.
+            /// </summary>
+            private XRHand? GetTrackedHand()
+            {
+                using (GetTrackedHandPerfMarker.Auto())
+                {
+                    if (xrHandSubsystem == null || !xrHandSubsystem.running)
+                    {
+                        xrHandSubsystem = XRSubsystemHelpers.GetFirstRunningSubsystem<XRHandSubsystem>();
+
+                        if (xrHandSubsystem == null)
+                        {
+                            // No hand subsystem running this frame.
+                            return null;
+                        }
+                    }
+
+                    XRHand hand = HandNode == XRNode.LeftHand ? xrHandSubsystem.leftHand : xrHandSubsystem.rightHand;
+                    return hand.isTracked ? hand : null;
+                }
+            }
+
+            private static readonly ProfilerMarker TryCalculateEntireHandPerfMarker =
+                new ProfilerMarker("[MRTK] UnityHandContainer.TryCalculateEntireHand");
+
+            /// <summary/>
+            /// For a certain hand, query every Bone in the hand, and write all results to the
+            /// handJoints collection. This will also mark handsQueriedThisFrame[handNode] = true.
+            /// </summary>
+            private void TryCalculateEntireHand()
+            {
+                using (TryCalculateEntireHandPerfMarker.Auto())
+                {
+                    hand = GetTrackedHand();
+
+                    if (!hand.HasValue)
+                    {
+                        // No articulated hand device available this frame.
+                        FullQueryValid = false;
+                        AlreadyFullQueried = true;
+                        return;
+                    }
+
+                    // Null checks against Unity objects can be expensive, especially when you do
+                    // it 52 times per frame (26 hand joints across 2 hands). Instead, we manage
+                    // the playspace transformation internally for hand joints.
+                    // Joints are relative to the camera floor offset object.
+                    Transform origin = PlayspaceUtilities.XROrigin.CameraFloorOffsetObject.transform;
+                    if (origin == null)
+                    {
+                        return;
+                    }
+
+                    FullQueryValid = true;
+
+                    for (int i = 0; i < (int)TrackedHandJoint.TotalJoints; i++)
+                    {
+                        FullQueryValid &= TryUpdateJoint(i, hand.Value.GetJoint(TrackedHandJointIndexToXRHandJointID[i]), origin);
+                    }
+
+                    // Mark this hand as having been fully queried this frame.
+                    // If any joint is queried again this frame, we'll reuse the
+                    // information to avoid extra work.
+                    AlreadyFullQueried = true;
+                }
+            }
+
+            private static readonly ProfilerMarker TryUpdateJointPerfMarker =
+                new ProfilerMarker("[MRTK] UnityHandContainer.TryUpdateJoint");
+
+            /// <summary/>
+            /// Given a destination jointID, apply the Bone info to the correct struct
+            /// in the handJoints collection.
+            /// </summary>
+            private bool TryUpdateJoint(int jointIndex, XRHandJoint xrHandJoint, Transform playspaceTransform)
+            {
+                using (TryUpdateJointPerfMarker.Auto())
+                {
+                    // Not obtaining a pose is a failure state...
+                    if (!xrHandJoint.TryGetPose(out Pose pose))
+                    {
+                        return false;
+                    }
+
+                    // ...but not obtaining a radius has a built-in fallback.
+                    if (!xrHandJoint.TryGetRadius(out float radius))
+                    {
+                        radius = HandsUtils.DefaultHandJointRadius;
+                    }
+
+                    handJoints[jointIndex] = new HandJointPose(
+                        playspaceTransform.TransformPoint(pose.position),
+                        playspaceTransform.rotation * pose.rotation,
+                        radius);
+
+                    return true;
+                }
+            }
+
+            private static readonly XRHandJointID[] TrackedHandJointIndexToXRHandJointID = new XRHandJointID[]
+            {
+                XRHandJointID.Palm,
+                XRHandJointID.Wrist,
+
+                XRHandJointID.ThumbMetacarpal,
+                XRHandJointID.ThumbProximal,
+                XRHandJointID.ThumbDistal,
+                XRHandJointID.ThumbTip,
+
+                XRHandJointID.IndexMetacarpal,
+                XRHandJointID.IndexProximal,
+                XRHandJointID.IndexIntermediate,
+                XRHandJointID.IndexDistal,
+                XRHandJointID.IndexTip,
+
+                XRHandJointID.MiddleMetacarpal,
+                XRHandJointID.MiddleProximal,
+                XRHandJointID.MiddleIntermediate,
+                XRHandJointID.MiddleDistal,
+                XRHandJointID.MiddleTip,
+
+                XRHandJointID.RingMetacarpal,
+                XRHandJointID.RingProximal,
+                XRHandJointID.RingIntermediate,
+                XRHandJointID.RingDistal,
+                XRHandJointID.RingTip,
+
+                XRHandJointID.LittleMetacarpal,
+                XRHandJointID.LittleProximal,
+                XRHandJointID.LittleIntermediate,
+                XRHandJointID.LittleDistal,
+                XRHandJointID.LittleTip,
+            };
+        }
+    }
+}
