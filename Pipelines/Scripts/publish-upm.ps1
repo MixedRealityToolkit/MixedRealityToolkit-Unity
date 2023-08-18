406139eea1a30da3d4fc3d6fbe785b2bac86537e
Scott Haynie <shaynie@microsoft.com>
2023-08-18 09:28:05 -0700

    Add the deprecated packages to the upm feed

--- Pipelines/Scripts/publish-upm.ps1
+++ Pipelines/Scripts/publish-upm.ps1
@@ -44,6 +44,15 @@ try {
         Write-Output "======================="
         npm publish $_
     }
+    
+    # Publish the deprecated package files
+    Get-ChildItem -Path $PackageDirectory -Filter "com.microsoft.mrtk.*.tgz" | ForEach-Object {
+        Write-Output "======================="
+        Write-Output "Publishing: $($_.Name) "
+        Write-Output "======================="
+        npm publish $_
+    }
+    
 }
 finally {
     if ($RegistryPath) {
