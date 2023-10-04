e2c3e55981876487e5e57c4f495f5aedcf8b52bf
David Kline <13281406+david-c-kline@users.noreply.github.com>
2023-10-04 10:37:22 -0700

    Update SliderSounds to use one AudioSource instead of two (#495)
    * unify slider audio sources, cache and restore the pitch shift value
    
    * update package versionsto match slidersounds and prefab updates
    
    * reserialize packages-lock with 2021.3.21f1
    
    * remodify/serialize prefabs with 2021.3.21f1
    
    * Update org.mixedrealitytoolkit.uxcore/Slider/SliderSounds.cs
    
    Co-authored-by: Kurtis <3580640+keveleigh@users.noreply.github.com>
    Signed-off-by: David Kline <13281406+david-c-kline@users.noreply.github.com>
    
    * pr feedback: code improvements
    
    ---------
    
    Signed-off-by: David Kline <13281406+david-c-kline@users.noreply.github.com>
    Co-authored-by: Kline, Dave (David Kline) <kline@qti.qualcomm.com>
    Co-authored-by: Kurtis <3580640+keveleigh@users.noreply.github.com>

--- org.mixedrealitytoolkit.uxcore/Slider/SliderSounds.cs
+++ org.mixedrealitytoolkit.uxcore/Slider/SliderSounds.cs
@@ -42,8 +42,6 @@ namespace MixedReality.Toolkit.UX
         /// </summary>
         public AudioClip InteractionEndSound { get => interactionEndSound; set => interactionEndSound = value; }
 
-
-
         [Header("Tick Notch Sounds")]
         [SerializeField]
         [Tooltip("Whether to play 'tick tick' sounds as the slider passes notches")]
@@ -110,30 +108,13 @@ namespace MixedReality.Toolkit.UX
         public float MinSecondsBetweenTicks { get => minSecondsBetweenTicks; set => minSecondsBetweenTicks = value; }
 
         [SerializeField]
-        [Tooltip("The source of the grab and release sounds")]
-        private AudioSource grabReleaseAudioSource = null;
+        [Tooltip("The AudioSource to be used when playing slider sounds.")]
+        private AudioSource audioSource = null;
 
         /// <summary>
-        /// Gets or sets the AudioSource used to play grab and release sounds.
+        /// Gets or sets the AudioSource used to play slider sounds.
         /// </summary>
-        public AudioSource GrabReleaseAudioSource
-        {
-            get => grabReleaseAudioSource;
-            set => grabReleaseAudioSource = value;
-        }
-
-        [SerializeField]
-        [Tooltip("The source of the pass notch sounds")]
-        private AudioSource passNotchAudioSource = null;
-
-        /// <summary>
-        /// Gets or sets the AudioSource used to play sound when the user moves the slider thumb past a notch.
-        /// </summary>
-        public AudioSource PassNotchAudioSource
-        {
-            get => passNotchAudioSource;
-            set => passNotchAudioSource = value;
-        }
+        public AudioSource AudioSource { get => audioSource; set => audioSource = value; }
 
         #region Private members
         private Slider slider;
@@ -151,13 +132,14 @@ namespace MixedReality.Toolkit.UX
         /// </summary> 
         private void Start()
         {
-            if (grabReleaseAudioSource == null)
+            // Ensure that we have a valid audio source to work with.
+            if (audioSource == null)
             {
-                grabReleaseAudioSource = gameObject.AddComponent<AudioSource>();
-            }
-            if (passNotchAudioSource == null)
-            {
-                passNotchAudioSource = gameObject.AddComponent<AudioSource>();
+                audioSource = gameObject.GetComponent<AudioSource>();
+                if (audioSource == null)
+                {
+                    audioSource = gameObject.AddComponent<AudioSource>();
+                }
             }
 
             slider = GetComponent<Slider>();
@@ -171,19 +153,22 @@ namespace MixedReality.Toolkit.UX
             slider.OnValueUpdated.AddListener(OnValueUpdated);
         }
 
+        private const float NeutralPitchShift = 1f;
+ 
         private void OnValueUpdated(SliderEventData eventData)
         {
-            if (!(playSoundsOnlyOnInteract && !isInteracting) && playTickSounds && passNotchAudioSource != null && passNotchSound != null)
+
+            if (!(playSoundsOnlyOnInteract && !isInteracting) && playTickSounds && AudioSource != null && passNotchSound != null)
             {
                 float delta = eventData.NewValue - eventData.OldValue;
                 accumulatedDeltaSliderValue += Mathf.Abs(delta);
                 var now = Time.timeSinceLevelLoad;
                 if (accumulatedDeltaSliderValue >= tickEvery && now - lastSoundPlayTime > minSecondsBetweenTicks)
                 {
-                    passNotchAudioSource.pitch = Mathf.Lerp(startPitch, endPitch, eventData.NewValue);
-                    if (passNotchAudioSource.isActiveAndEnabled)
+                    AudioSource.pitch = Mathf.Lerp(startPitch, endPitch, eventData.NewValue);
+                    if (AudioSource.isActiveAndEnabled)
                     {
-                        passNotchAudioSource.PlayOneShot(passNotchSound);
+                        AudioSource.PlayOneShot(passNotchSound);
                     }
 
                     accumulatedDeltaSliderValue = 0;
@@ -195,21 +180,23 @@ namespace MixedReality.Toolkit.UX
         private void OnInteractionStarted(SelectEnterEventArgs args)
         {
             isInteracting = true;
-            if (interactionStartSound != null && grabReleaseAudioSource != null && grabReleaseAudioSource.isActiveAndEnabled)
+            if (interactionStartSound != null && AudioSource != null && AudioSource.isActiveAndEnabled)
             {
-                grabReleaseAudioSource.PlayOneShot(interactionStartSound);
+                // Moving the slider destructively changes the pitch, reset to non-shifted
+                AudioSource.pitch = NeutralPitchShift;
+                AudioSource.PlayOneShot(interactionStartSound);
             }
         }
 
         private void OnInteractionEnded(SelectExitEventArgs args)
         {
             isInteracting = false;
-            if (interactionEndSound != null && grabReleaseAudioSource != null && grabReleaseAudioSource.isActiveAndEnabled)
+            if (interactionEndSound != null && AudioSource != null && AudioSource.isActiveAndEnabled)
             {
-                grabReleaseAudioSource.PlayOneShot(interactionEndSound);
+                // Moving the slider destructively changes the pitch, reset to non-shifted
+                AudioSource.pitch = NeutralPitchShift;
+                AudioSource.PlayOneShot(interactionEndSound);
             }
         }
     }
-
-
 }
