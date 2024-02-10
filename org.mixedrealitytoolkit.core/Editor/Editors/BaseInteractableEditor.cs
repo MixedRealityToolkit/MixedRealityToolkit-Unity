f3e03b8229c601fc970a9fcfa0e9db23cd7ad890
Risto Rangel Kuoppa <risto.rangelkuoppa@microsoft.com>
2024-02-09 19:08:14 -0800

    Making Proximity-Hover events appear in Editor's Inspector under 'MRTK Events'>'Proximity-Hover Events'

--- org.mixedrealitytoolkit.core/Editor/Editors/BaseInteractableEditor.cs
+++ org.mixedrealitytoolkit.core/Editor/Editors/BaseInteractableEditor.cs
@@ -27,6 +27,9 @@ namespace MixedReality.Toolkit.Editor
         private SerializedProperty isActiveHovered;
         private SerializedProperty disabledInteractorTypes;
 
+        private SerializedProperty FirstProximityHoverEntered;
+        private SerializedProperty LastProximityHoverEntered;
+
         private bool xriExpanded = false;
         private bool mrtkExpanded = false;
 
@@ -52,6 +55,9 @@ namespace MixedReality.Toolkit.Editor
             isPokeSelected = SetUpProperty(nameof(isPokeSelected));
 
             disabledInteractorTypes = SetUpProperty(nameof(disabledInteractorTypes));
+
+            FirstProximityHoverEntered = SetUpAutoProperty(nameof(FirstProximityHoverEntered));
+            LastProximityHoverEntered = SetUpAutoProperty(nameof(LastProximityHoverEntered));
         }
 
         /// <summary>
@@ -150,6 +156,17 @@ namespace MixedReality.Toolkit.Editor
                 DrawTimedFlag(isRaySelected, interactable.IsRaySelected, previousGUIColor, Color.cyan);
                 DrawTimedFlag(isGazePinchSelected, interactable.IsGazePinchSelected, previousGUIColor, Color.cyan);
                 DrawTimedFlag(isPokeSelected, interactable.IsPokeSelected, previousGUIColor, Color.cyan);
+            }
+
+            EditorGUILayout.Space();
+
+            using (new EditorGUILayout.VerticalScope(EditorStyles.helpBox))
+            {
+                EditorGUILayout.LabelField("Proximity-Hover Events", EditorStyles.boldLabel);
+                EditorGUILayout.Space();
+
+                EditorGUILayout.PropertyField(FirstProximityHoverEntered);
+                EditorGUILayout.PropertyField(LastProximityHoverEntered);
             }
         }
 
