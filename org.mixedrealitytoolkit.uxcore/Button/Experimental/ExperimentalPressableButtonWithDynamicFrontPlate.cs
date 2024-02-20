ef8dde61dfacecfe44bfa233cf753aa0991ed2a5
Risto Rangel Kuoppa <risto.rangelkuoppa@microsoft.com>
2024-02-20 11:50:43 -0800

    Adding remarks

--- org.mixedrealitytoolkit.uxcore/Button/Experimental/ExperimentalPressableButtonWithDynamicFrontPlate.cs
+++ org.mixedrealitytoolkit.uxcore/Button/Experimental/ExperimentalPressableButtonWithDynamicFrontPlate.cs
@@ -9,6 +9,14 @@ namespace MixedReality.Toolkit.UX
     /// <summary>
     /// This mono behaviour is used to enable or disable the front plate of a button when the button enters or exists proximity-hovering.
     /// </summary>
+    /// <remarks>
+    /// This is an experimental feature. This class is early in the cycle, it has
+    /// been labeled as experimental to indicate that it is still evolving, and
+    /// subject to change over time. Parts of the MRTK, such as this class, appear
+    /// to have a lot of value even if the details haven't fully been fleshed out.
+    /// For these types of features, we want the community to see them and get
+    /// value out of them early enough so to provide feedback.
+    /// </remarks>
     [RequireComponent(typeof(PressableButton))]
     public class ExperimentalPressableButtonWithDynamicFrontPlate : MonoBehaviour
     {
