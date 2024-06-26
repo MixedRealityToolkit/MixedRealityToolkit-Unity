535fcf22b404f5664091575fb26c7591a77eba57
Risto Rangel <risto.rangelkuoppa@microsoft.com>
2024-06-26 11:47:03 -0800

    Adding remaining SolverHandlerTests to be tested with the new controllerless prefabs

--- org.mixedrealitytoolkit.spatialmanipulation/Tests/Runtime/SolverHandlerTestsForControllerlessRig.cs
+++ org.mixedrealitytoolkit.spatialmanipulation/Tests/Runtime/SolverHandlerTestsForControllerlessRig.cs
@@ -11,6 +11,7 @@ using NUnit.Framework;
 using System.Collections;
 using UnityEngine;
 using UnityEngine.TestTools;
+using UnityEngine.XR;
 
 namespace MixedReality.Toolkit.SpatialManipulation.Runtime.Tests
 {
@@ -402,6 +403,315 @@ namespace MixedReality.Toolkit.SpatialManipulation.Runtime.Tests
 
             // Check that the SolverHandler keeps tracking the right hand
             Assert.IsTrue(solverHandler.TransformTarget.position == solverHandler.RightInteractor.transform.position, $"Solver Handler switched to wrong active hand");
+        }
+
+        /// <summary>
+        /// This checks if the SolverHandler moves with head when tracking the head
+        /// </summary>
+        [UnityTest]
+        public IEnumerator SolverHandlerHeadMovesWithHead()
+        {
+            // Disable gaze interactions for this unit test;
+            InputTestUtilities.DisableGazeInteractor();
+
+            // Set up GameObject with a SolverHandler
+            var testObject = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            var solverHandler = testObject.AddComponent<SolverHandler>();
+
+            // Set it to track head
+            solverHandler.TrackedTargetType = TrackedObjectType.Head;
+
+            yield return new WaitForFixedUpdate();
+            yield return null;
+
+            Camera.main.transform.position = new Vector3(0.1f, 0.1f, 0.1f);
+            yield return RuntimeTestUtilities.WaitForUpdates();
+
+            // Check if SolverHandler starts at camera pose
+            Assert.IsTrue(solverHandler.TransformTarget.position == Camera.main.transform.position, $"Solver Handler not tracking head");
+
+            Camera.main.transform.position = new Vector3(1f, 1f, 1f);
+            yield return RuntimeTestUtilities.WaitForUpdates();
+
+            // Check if SolverHandler starts at camera pose
+            Assert.IsTrue(solverHandler.TransformTarget.position == Camera.main.transform.position, $"Solver Handler not moving with head");
+        }
+
+        /// <summary>
+        /// This checks if the SolverHandler correctly applies additional offset and rotation
+        /// </summary>
+        [UnityTest]
+        public IEnumerator SolverHandlerAppliesOffset()
+        {
+            // Disable gaze interactions for this unit test;
+            InputTestUtilities.DisableGazeInteractor();
+
+            // Set up GameObject with a SolverHandler
+            var testObject = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            var solverHandler = testObject.AddComponent<SolverHandler>();
+
+            // Set it to track head
+            solverHandler.TrackedTargetType = TrackedObjectType.Head;
+
+            // Apply additional offsets
+            solverHandler.AdditionalOffset = Vector3.one;
+            solverHandler.AdditionalRotation = new Vector3(30f, 30f, 30f);
+
+            yield return new WaitForFixedUpdate();
+            yield return null;
+
+            Camera.main.transform.position = new Vector3(0.1f, 0.1f, 0.1f);
+            yield return RuntimeTestUtilities.WaitForUpdates();
+
+            // Check if SolverHandler is at expected position and rotation
+            Vector3 expectedPos = Camera.main.transform.position + solverHandler.AdditionalOffset;
+            Quaternion expectedDir = Camera.main.transform.rotation * Quaternion.Euler(30f, 30f, 30f);
+            Assert.IsTrue(solverHandler.TransformTarget.position == expectedPos, $"Solver Handler not applying additional offset");
+            Assert.IsTrue(solverHandler.TransformTarget.rotation == expectedDir, $"Solver Handler not applying additional rotation");
+        }
+
+        /// <summary>
+        /// This checks if the SolverHandler correctly switches to the active hand joint when tracking
+        /// two hands
+        /// </summary>
+        [UnityTest]
+        public IEnumerator SolverHandlerHandJointSwitchesToActiveHand()
+        {
+            // Disable gaze interactions for this unit test;
+            InputTestUtilities.DisableGazeInteractor();
+
+            // For tracking joint pose in test
+            HandJointPose jointPose = new HandJointPose();
+
+            // Set up GameObject with a SolverHandler
+            var testObject = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            var solverHandler = testObject.AddComponent<SolverHandler>();
+            var joint = TrackedHandJoint.Palm;
+
+            // Set it to track interactors
+            solverHandler.TrackedHandedness = Handedness.Both;
+            solverHandler.TrackedTargetType = TrackedObjectType.HandJoint;
+            solverHandler.TrackedHandJoint = joint;
+
+            yield return RuntimeTestUtilities.WaitForUpdates();
+
+            TestHand rightHand = new TestHand(Handedness.Right);
+            TestHand leftHand = new TestHand(Handedness.Left);
+            XRNode rightHandNode = Handedness.Right.ToXRNode().Value;
+            XRNode leftHandNode = Handedness.Left.ToXRNode().Value;
+            var initialHandPosition = InputTestUtilities.InFrontOfUser(0.5f);
+
+            yield return rightHand.Show(initialHandPosition);
+            yield return RuntimeTestUtilities.WaitForUpdates();
+
+            // Check if SolverHandler starts with target on right hand joint
+            XRSubsystemHelpers.HandsAggregator.TryGetJoint(joint, rightHandNode, out jointPose);
+            Assert.IsTrue(solverHandler.TransformTarget.position == jointPose.Position, $"Solver Handler started tracking incorrect hand joint");
+
+            // Hide the right hand and make the left hand active at a new position
+            yield return rightHand.Hide();
+            yield return RuntimeTestUtilities.WaitForUpdates();
+            var secondHandPosition = new Vector3(-0.05f, -0.05f, 1f);
+            yield return leftHand.Show(secondHandPosition);
+            yield return RuntimeTestUtilities.WaitForUpdates();
+
+            // Check if the SolverHandler moves the target to the left hand joint
+            XRSubsystemHelpers.HandsAggregator.TryGetJoint(joint, leftHandNode, out jointPose);
+            Assert.IsTrue(solverHandler.TransformTarget.position == jointPose.Position, $"Solver Handler did not switch to active hand joint");
+
+            // Repeat the test, but hide the left hand this time
+            yield return leftHand.Hide();
+            yield return RuntimeTestUtilities.WaitForUpdates();
+            Vector3 finalPosition = InputTestUtilities.InFrontOfUser(new Vector3(0.05f, 0.05f, 0.5f));
+            yield return rightHand.Show(finalPosition);
+            yield return RuntimeTestUtilities.WaitForUpdates();
+
+            // Check if the SolverHandler moves the target back to the right hand joint
+            XRSubsystemHelpers.HandsAggregator.TryGetJoint(joint, rightHandNode, out jointPose);
+            Assert.IsTrue(solverHandler.TransformTarget.position == jointPose.Position, $"Solver Handler did not switch to final hand joint");
+        }
+
+        /// <summary>
+        /// This checks if the SolverHandler correctly switches to the active hand joint when tracking
+        /// two hands, when the serialized `TrackedHandedness` value to set to Unity's
+        /// Everything value, with is -1 or 0xFFFFFFFF. Everything can be set via Unity's
+        /// inspector window.
+        /// </summary>
+        [UnityTest]
+        public IEnumerator SolverHandlerHandJointSwitchesToActiveHandWithEverythingValue()
+        {
+            // Disable gaze interactions for this unit test;
+            InputTestUtilities.DisableGazeInteractor();
+
+            // For tracking joint pose in test
+            HandJointPose jointPose = new HandJointPose();
+
+            // Set up GameObject with a SolverHandler
+            var testObject = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            var solverHandler = testObject.AddComponent<SolverHandler>();
+            var joint = TrackedHandJoint.Palm;
+
+            // Set it to track interactors
+            solverHandler.TrackedHandedness = Handedness.Both;
+            solverHandler.TrackedTargetType = TrackedObjectType.HandJoint;
+            solverHandler.TrackedHandJoint = joint;
+
+            yield return RuntimeTestUtilities.WaitForUpdates();
+
+            TestHand rightHand = new TestHand(Handedness.Right);
+            TestHand leftHand = new TestHand(Handedness.Left);
+            XRNode rightHandNode = Handedness.Right.ToXRNode().Value;
+            XRNode leftHandNode = Handedness.Left.ToXRNode().Value;
+            var initialHandPosition = InputTestUtilities.InFrontOfUser(0.5f);
+
+            yield return rightHand.Show(initialHandPosition);
+            yield return RuntimeTestUtilities.WaitForUpdates();
+
+            // Check if SolverHandler starts with target on right hand joint
+            XRSubsystemHelpers.HandsAggregator.TryGetJoint(joint, rightHandNode, out jointPose);
+            Assert.IsTrue(solverHandler.TransformTarget.position == jointPose.Position, $"Solver Handler started tracking incorrect hand joint");
+
+            // Hide the right hand and make the left hand active at a new position
+            yield return rightHand.Hide();
+            yield return RuntimeTestUtilities.WaitForUpdates();
+            var secondHandPosition = new Vector3(-0.05f, -0.05f, 1f);
+            yield return leftHand.Show(secondHandPosition);
+            yield return RuntimeTestUtilities.WaitForUpdates();
+
+            // Check if the SolverHandler moves the target to the left hand joint
+            XRSubsystemHelpers.HandsAggregator.TryGetJoint(joint, leftHandNode, out jointPose);
+            Assert.IsTrue(solverHandler.TransformTarget.position == jointPose.Position, $"Solver Handler did not switch to active hand joint");
+
+            // Repeat the test, but hide the left hand this time
+            yield return leftHand.Hide();
+            yield return RuntimeTestUtilities.WaitForUpdates();
+            Vector3 finalPosition = InputTestUtilities.InFrontOfUser(new Vector3(0.05f, 0.05f, 0.5f));
+            yield return rightHand.Show(finalPosition);
+            yield return RuntimeTestUtilities.WaitForUpdates();
+
+            // Check if the SolverHandler moves the target back to the right hand joint
+            XRSubsystemHelpers.HandsAggregator.TryGetJoint(joint, rightHandNode, out jointPose);
+            Assert.IsTrue(solverHandler.TransformTarget.position == jointPose.Position, $"Solver Handler did not switch to final hand joint");
+        }
+
+        /// <summary>
+        /// This checks if the SolverHandler can be configured to only track left hand joint only
+        /// </summary>
+        [UnityTest]
+        public IEnumerator SolverHandlerHandJointLeftHandOnly()
+        {
+            // Disable gaze interactions for this unit test;
+            InputTestUtilities.DisableGazeInteractor();
+
+            // For tracking joint pose in test
+            HandJointPose jointPose = new HandJointPose();
+
+            // Set up GameObject with a SolverHandler
+            var testObject = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            var solverHandler = testObject.AddComponent<SolverHandler>();
+            var joint = TrackedHandJoint.Palm;
+
+            // Set it to track interactors
+            solverHandler.TrackedHandedness = Handedness.Left;
+            solverHandler.TrackedTargetType = TrackedObjectType.HandJoint;
+            solverHandler.TrackedHandJoint = joint;
+
+            yield return RuntimeTestUtilities.WaitForUpdates();
+
+            TestHand rightHand = new TestHand(Handedness.Right);
+            TestHand leftHand = new TestHand(Handedness.Left);
+            XRNode rightHandNode = Handedness.Right.ToXRNode().Value;
+            XRNode leftHandNode = Handedness.Left.ToXRNode().Value;
+            var initialHandPosition = InputTestUtilities.InFrontOfUser(0.5f);
+
+            yield return rightHand.Show(initialHandPosition);
+            yield return RuntimeTestUtilities.WaitForUpdates();
+
+            // Check if SolverHandler did not start with target on right hand joint
+            XRSubsystemHelpers.HandsAggregator.TryGetJoint(joint, rightHandNode, out jointPose);
+            Assert.IsTrue(solverHandler.TransformTarget.position != jointPose.Position, $"Solver Handler started tracking incorrect hand joint");
+
+            // Hide the right hand and make the left hand active at a new position
+            yield return rightHand.Hide();
+            yield return RuntimeTestUtilities.WaitForUpdates();
+            var secondHandPosition = new Vector3(-0.05f, -0.05f, 1f);
+            yield return leftHand.Show(secondHandPosition);
+            yield return RuntimeTestUtilities.WaitForUpdates();
+
+            // Check if the SolverHandler moves the target to the left hand joint
+            XRSubsystemHelpers.HandsAggregator.TryGetJoint(joint, leftHandNode, out jointPose);
+            Assert.IsTrue(solverHandler.TransformTarget.position == jointPose.Position, $"Solver Handler did not start to track correct hand joint");
+
+            // Repeat the test, but hide the left hand this time
+            yield return leftHand.Hide();
+            yield return RuntimeTestUtilities.WaitForUpdates();
+            Vector3 finalPosition = InputTestUtilities.InFrontOfUser(new Vector3(0.05f, 0.05f, 0.5f));
+            yield return rightHand.Show(finalPosition);
+            yield return RuntimeTestUtilities.WaitForUpdates();
+
+            // Check if the SolverHandler did not moves the target to the right hand joint
+            XRSubsystemHelpers.HandsAggregator.TryGetJoint(joint, rightHandNode, out jointPose);
+            Assert.IsTrue(solverHandler.TransformTarget.position != jointPose.Position, $"Solver Handler switched to incorrect hand joint");
+        }
+
+        /// <summary>
+        /// This checks if the SolverHandler can be configured to only track right hand only
+        /// </summary>
+        [UnityTest]
+        public IEnumerator SolverHandlerHandJointRightHandOnly()
+        {
+            // Disable gaze interactions for this unit test;
+            InputTestUtilities.DisableGazeInteractor();
+
+            // For tracking joint pose in test
+            HandJointPose jointPose = new HandJointPose();
+
+            // Set up GameObject with a SolverHandler
+            var testObject = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            var solverHandler = testObject.AddComponent<SolverHandler>();
+            var joint = TrackedHandJoint.Palm;
+
+            // Set it to track interactors
+            solverHandler.TrackedHandedness = Handedness.Right;
+            solverHandler.TrackedTargetType = TrackedObjectType.HandJoint;
+            solverHandler.TrackedHandJoint = joint;
+
+            yield return RuntimeTestUtilities.WaitForUpdates();
+
+            TestHand rightHand = new TestHand(Handedness.Right);
+            TestHand leftHand = new TestHand(Handedness.Left);
+            XRNode rightHandNode = Handedness.Right.ToXRNode().Value;
+            XRNode leftHandNode = Handedness.Left.ToXRNode().Value;
+            var initialHandPosition = InputTestUtilities.InFrontOfUser(0.5f);
+
+            yield return leftHand.Show(initialHandPosition);
+            yield return RuntimeTestUtilities.WaitForUpdates();
+
+            // Check if SolverHandler did not start with target on left hand joint
+            XRSubsystemHelpers.HandsAggregator.TryGetJoint(joint, leftHandNode, out jointPose);
+            Assert.IsTrue(solverHandler.TransformTarget.position != jointPose.Position, $"Solver Handler started tracking incorrect hand joint");
+
+            // Hide the left hand and make the right hand active at a new position
+            yield return leftHand.Hide();
+            yield return RuntimeTestUtilities.WaitForUpdates();
+            var secondHandPosition = new Vector3(-0.05f, -0.05f, 1f);
+            yield return rightHand.Show(secondHandPosition);
+            yield return RuntimeTestUtilities.WaitForUpdates();
+
+            // Check if the SolverHandler moves the target to the right hand joint
+            XRSubsystemHelpers.HandsAggregator.TryGetJoint(joint, rightHandNode, out jointPose);
+            Assert.IsTrue(solverHandler.TransformTarget.position == jointPose.Position, $"Solver Handler did not start to track correct hand joint");
+
+            // Repeat the test, but hide the right hand this time
+            yield return rightHand.Hide();
+            yield return RuntimeTestUtilities.WaitForUpdates();
+            Vector3 finalPosition = InputTestUtilities.InFrontOfUser(new Vector3(0.05f, 0.05f, 0.5f));
+            yield return leftHand.Show(finalPosition);
+            yield return RuntimeTestUtilities.WaitForUpdates();
+
+            // Check if the SolverHandler did not moves the target to the left hand joint
+            XRSubsystemHelpers.HandsAggregator.TryGetJoint(joint, leftHandNode, out jointPose);
+            Assert.IsTrue(solverHandler.TransformTarget.position != jointPose.Position, $"Solver Handler switched to incorrect hand joint");
         }
     }
 }
