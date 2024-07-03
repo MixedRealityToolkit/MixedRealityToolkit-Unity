a3bdf6e4e3d177e520cc01bf76f0cecd8a2b6e35
Adam Mollis <amollis@microsoft.com>
2024-07-03 14:41:36 -0700

    test if these test failures is a timing thing

--- org.mixedrealitytoolkit.uxcomponents/Tests/Runtime/SeeItSayItCanvasTests.cs
+++ org.mixedrealitytoolkit.uxcomponents/Tests/Runtime/SeeItSayItCanvasTests.cs
@@ -86,8 +86,9 @@ namespace MixedReality.Toolkit.UX.Runtime.Tests
             yield return hand.Show(Vector3.one);
             yield return RuntimeTestUtilities.WaitForUpdates();
             yield return hand.MoveTo(testButton.transform.position);
+
             // Label show animation takes 1.0 seconds.  Wait for it to finish.
-            yield return new WaitForSecondsRealtime(1.25f);
+            yield return new WaitForSecondsRealtime(2.25f);
             Assert.IsTrue(labelChild?.activeInHierarchy == true, $"The label is enabled when the button is hovered.");
 
             // Move hand away from the object
