7a0697afc87a866b4e7a68725573be04aa18cec4
Kurtis <quic_keveleig@quicinc.com>
2025-01-09 11:33:06 -0800

    Create MRTKFiles

---
+++ org.mixedrealitytoolkit.core/Editor/MRTKFiles.cs
@@ -0,0 +1,93 @@
+// Copyright (c) Mixed Reality Toolkit Contributors
+// Licensed under the BSD 3-Clause
+
+using System.IO;
+using System.Linq;
+using UnityEditor;
+using UnityEngine;
+
+namespace MixedReality.Toolkit.Editor
+{
+    public class MRTKFiles
+    {
+        private const string GeneratedName = "MRTK.Generated";
+        private const string GeneratedSentinelFileName = GeneratedName + ".sentinel";
+
+        private static readonly string DefaultGeneratedFolderPath = Path.Combine("Assets", GeneratedName);
+        private static readonly string DefaultSentinelFilePath = Path.Combine(DefaultGeneratedFolderPath, GeneratedSentinelFileName);
+        private static string generatedFolderPath = string.Empty;
+
+        public static string GeneratedFolderPath
+        {
+            get
+            {
+                if (string.IsNullOrWhiteSpace(generatedFolderPath))
+                {
+                    foreach (string guid in AssetDatabase.FindAssets(GeneratedName))
+                    {
+                        string path = AssetDatabase.GUIDToAssetPath(guid);
+                        if (path.Contains(GeneratedSentinelFileName))
+                        {
+                            generatedFolderPath = Path.GetDirectoryName(path);
+                            return generatedFolderPath;
+                        }
+                    }
+
+
+                    //string[] filePathResults = Directory.GetFiles(Application.dataPath, GeneratedSentinelFileName, SearchOption.AllDirectories);
+                    //if (filePathResults.Length > 0)
+                    //{
+                    //    generatedFolderPath = Path.GetDirectoryName(filePathResults[0]);
+                    //    if (filePathResults.Length > 1)
+                    //    {
+                    //        Debug.LogWarning($"{filePathResults.Length} MRTK.Generated sentinels were found.\nUsing the one in {generatedFolderPath}");
+                    //    }
+                    //}
+                    //else
+                    //{
+                        if (!Directory.Exists(DefaultGeneratedFolderPath))
+                        {
+                            Directory.CreateDirectory(DefaultGeneratedFolderPath);
+                        }
+
+                        if (!File.Exists(DefaultSentinelFilePath))
+                        {
+                            // Make sure we create and dispose/close the filestream just created
+                            using (FileStream f = File.Create(DefaultSentinelFilePath)) { }
+                        }
+                        generatedFolderPath = DefaultGeneratedFolderPath;
+                    //}
+                }
+                return generatedFolderPath;
+            }
+        }
+
+        private class AssetPostprocessor : UnityEditor.AssetPostprocessor
+        {
+            public static void OnPostprocessAllAssets(string[] importedAssets, string[] deletedAssets, string[] movedAssets, string[] movedFromAssetPaths)
+            {
+                foreach (string asset in deletedAssets.Concat(movedFromAssetPaths))
+                {
+                    if (Path.GetFileName(asset) == GeneratedSentinelFileName && Path.GetDirectoryName(asset) == generatedFolderPath)
+                    {
+                        generatedFolderPath = string.Empty;
+                    }
+                }
+
+                foreach (string asset in importedAssets.Concat(movedAssets))
+                {
+                    if (Path.GetFileName(asset) == GeneratedSentinelFileName)
+                    {
+                        string newPath = Path.GetDirectoryName(asset);
+                        if (generatedFolderPath != string.Empty && generatedFolderPath != newPath)
+                        {
+                            Debug.LogWarning($"Previous MRTK.Generated folder was not unregistered properly: {generatedFolderPath}.\nReplacing with {newPath}");
+                        }
+                        Debug.Log($"Found MRTK.Generated at {newPath}.");
+                        generatedFolderPath = newPath;
+                    }
+                }
+            }
+        }
+    }
+}
