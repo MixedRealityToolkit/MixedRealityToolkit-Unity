86746f44d53c0efd17e68f647bf856b86ced2d8d
Kurtis <quic_keveleig@quicinc.com>
2025-01-10 14:39:33 -0800

    Update VirtualizedScrollRectListTester.cs

--- UnityProjects/MRTKDevTemplate/Assets/Scripts/VirtualizedScrollRectListTester.cs
+++ UnityProjects/MRTKDevTemplate/Assets/Scripts/VirtualizedScrollRectListTester.cs
@@ -26,7 +26,7 @@ namespace MixedReality.Toolkit.Examples.Demos
         /// <summary>
         /// A Unity event function that is called on the frame when a script is enabled just before any of the update methods are called the first time.
         /// </summary> 
-        private void OnEnable()
+        private void Start()
         {
             list = GetComponent<VirtualizedScrollRectList>();
             list.OnVisible = (go, i) =>
