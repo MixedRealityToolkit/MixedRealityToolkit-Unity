cef626584bc1ff30aaded5d28b2368bd09ad2eda
Kurtis <quic_keveleig@quicinc.com>
2025-02-27 15:33:12 -0800

    Initial drop

---
+++ org.mixedrealitytoolkit.input/Visualizers/HandMeshVisualizer.cs
@@ -0,0 +1,42 @@
+// Copyright (c) Mixed Reality Toolkit Contributors
+// Licensed under the BSD 3-Clause
+
+using UnityEngine;
+using UnityEngine.XR;
+
+namespace MixedReality.Toolkit.Input
+{
+    public abstract class HandMeshVisualizer : MonoBehaviour
+    {
+        [SerializeField]
+        [Tooltip("The XRNode on which this hand is located.")]
+        private XRNode handNode = XRNode.LeftHand;
+
+        /// <summary> The XRNode on which this hand is located. </summary>
+        public XRNode HandNode { get => handNode; set => handNode = value; }
+
+        [SerializeField]
+        [Tooltip("When true, this visualizer will render rigged hands even on XR devices " +
+                 "with transparent displays. When false, the rigged hands will only render " +
+                 "on devices with opaque displays.")]
+        private bool showHandsOnTransparentDisplays;
+
+        /// <summary>
+        /// When true, this visualizer will render rigged hands even on XR devices with transparent displays.
+        /// When false, the rigged hands will only render on devices with opaque displays.
+        /// Usually, it's recommended not to show hand visualization on transparent displays as it can
+        /// distract from the user's real hands, and cause a "double image" effect that can be disconcerting.
+        /// </summary>
+        public bool ShowHandsOnTransparentDisplays
+        {
+            get => showHandsOnTransparentDisplays;
+            set => showHandsOnTransparentDisplays = value;
+        }
+
+        protected virtual void OnEnable()
+        {
+            Debug.Assert(handNode == XRNode.LeftHand || handNode == XRNode.RightHand,
+                $"HandVisualizer has an invalid XRNode ({handNode})!");
+        }
+    }
+}
