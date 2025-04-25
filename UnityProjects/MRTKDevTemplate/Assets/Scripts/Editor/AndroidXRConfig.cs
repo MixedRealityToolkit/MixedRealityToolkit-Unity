bbd9c9c47b7aa51f42324d3dcd5d97ebad7d2c2a
Kurtis <keveleig@qti.qualcomm.com>
2025-04-25 09:44:02 -0700

    Fix code validation issues

--- UnityProjects/MRTKDevTemplate/Assets/Scripts/Editor/AndroidXRConfig.cs
+++ UnityProjects/MRTKDevTemplate/Assets/Scripts/Editor/AndroidXRConfig.cs
@@ -8,33 +8,36 @@ using UnityEditor.PackageManager.Requests;
 using UnityEngine;
 #endif
 
-internal class AndroidXRConfig
+namespace MixedReality.Toolkit.Examples.Demos.Editor
 {
+    internal class AndroidXRConfig
+    {
 #if UNITY_6000_0_OR_NEWER
-    private static AddAndRemoveRequest request;
+        private static AddAndRemoveRequest request;
 
-    [MenuItem("Mixed Reality/MRTK3/Examples/Configure for Android XR...", priority = int.MaxValue)]
-    public static void InstallPackages()
-    {
-        // Already a request in progress, so don't re-run
-        if (request != null)
+        [MenuItem("Mixed Reality/MRTK3/Examples/Configure for Android XR...", priority = int.MaxValue)]
+        public static void InstallPackages()
         {
-            return;
+            // Already a request in progress, so don't re-run
+            if (request != null)
+            {
+                return;
+            }
+
+            Debug.Log("Adding com.unity.xr.androidxr-openxr and com.google.xr.extensions...");
+            request = Client.AddAndRemove(new[] { "com.unity.xr.androidxr-openxr", "https://github.com/android/android-xr-unity-package.git" });
+            EditorApplication.update += Progress;
         }
 
-        Debug.Log("Adding com.unity.xr.androidxr-openxr and com.google.xr.extensions...");
-        request = Client.AddAndRemove(new[] { "com.unity.xr.androidxr-openxr", "https://github.com/android/android-xr-unity-package.git" });
-        EditorApplication.update += Progress;
-    }
-
-    private static void Progress()
-    {
-        if (request.IsCompleted)
+        private static void Progress()
         {
-            Debug.Log($"Package install request complete ({request.Status})");
-            EditorApplication.update -= Progress;
-            request = null;
+            if (request.IsCompleted)
+            {
+                Debug.Log($"Package install request complete ({request.Status})");
+                EditorApplication.update -= Progress;
+                request = null;
+            }
         }
-    }
 #endif
+    }
 }
