cf78a599ce729a52227a293c68f88bc60617ca2f
Kurtis <keveleig@qti.qualcomm.com>
2025-07-09 09:57:53 -0700

    Revert "Add new input action focus handling (#1028)"
    This reverts commit f52cdedaa3dbf8b272851a3c2f63f67ae8d743c4.

--- org.mixedrealitytoolkit.core/Subsystems/MRTKLifecycleManager.cs
+++ org.mixedrealitytoolkit.core/Subsystems/MRTKLifecycleManager.cs
@@ -5,7 +5,6 @@ using System;
 using System.Collections.Generic;
 using Unity.Profiling;
 using UnityEngine;
-using UnityEngine.InputSystem;
 
 namespace MixedReality.Toolkit.Subsystems
 {
@@ -19,9 +18,6 @@ namespace MixedReality.Toolkit.Subsystems
         MonoBehaviour,
         IDisposable
     {
-        [SerializeField, Tooltip("A set of input actions to enable/disable according to the app's focus state.")]
-        private InputActionReference[] inputActionReferences;
-
         private List<IMRTKManagedSubsystem> managedSubsystems = new List<IMRTKManagedSubsystem>();
 
         /// <summary>
@@ -176,33 +172,6 @@ namespace MixedReality.Toolkit.Subsystems
                 foreach (IMRTKManagedSubsystem subsystem in managedSubsystems)
                 {
                     subsystem.LateUpdate();
-                }
-            }
-        }
-
-        /// <summary>
-        /// Sent to all GameObjects when the player gets or loses focus.
-        /// </summary>
-        /// <param name="focus"><see langword="true"/> if the GameObjects have focus, else <see langword="false"/>.</param>
-        protected void OnApplicationFocus(bool focus)
-        {
-            // We want to ensure we're focused for input, as some runtimes continue reporting "tracked" while pose updates are paused.
-            // This is allowed, per-spec, as a "should": "Runtimes should make input actions inactive while the application is unfocused,
-            // and applications should react to an inactive input action by skipping rendering of that action's input avatar
-            // (depictions of hands or other tracked objects controlled by the user)."
-
-            if (focus)
-            {
-                foreach (InputActionReference reference in inputActionReferences)
-                {
-                    reference.action.Enable();
-                }
-            }
-            else
-            {
-                foreach (InputActionReference reference in inputActionReferences)
-                {
-                    reference.action.Disable();
                 }
             }
         }
