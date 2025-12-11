0bb45cc899c5e930660cb5197b311b10864101c4
Kurtis <keveleig@qti.qualcomm.com>
2025-12-11 12:49:35 -0800

    Update IGazePinchInteractor.cs

--- org.mixedrealitytoolkit.core/Interactors/IGazePinchInteractor.cs
+++ org.mixedrealitytoolkit.core/Interactors/IGazePinchInteractor.cs
@@ -1,15 +1,12 @@
 // Copyright (c) Mixed Reality Toolkit Contributors
 // Licensed under the BSD 3-Clause
 
-using UnityEngine.XR.Interaction.Toolkit.Interactors;
-
 namespace MixedReality.Toolkit
 {
     /// <summary>
     /// An interface that all gaze-pinch-like interactors must implement.
     /// </summary>
     public interface IGazePinchInteractor :
-        IXRInteractionStrengthInteractor,
 #pragma warning disable CS0618 // Type or member is obsolete
         IVariableSelectInteractor
 #pragma warning restore CS0618 // Type or member is obsolete
