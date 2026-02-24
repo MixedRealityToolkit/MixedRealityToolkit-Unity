7b69e099c610fdc857d5c5343caa4088e0f52048
Kurtis <3580640+keveleigh@users.noreply.github.com>
2026-02-23 17:39:25 -0800

    Iterate UWP eye calibration checker (#1106)

--- org.mixedrealitytoolkit.input/Utilities/EyeCalibration/EyeCalibrationChecker.cs
+++ org.mixedrealitytoolkit.input/Utilities/EyeCalibration/EyeCalibrationChecker.cs
@@ -1,11 +1,12 @@
 // Copyright (c) Mixed Reality Toolkit Contributors
 // Licensed under the BSD 3-Clause
 
-using System;
-using System.Runtime.InteropServices.WindowsRuntime;
 using UnityEngine;
 using UnityEngine.Events;
-#if WINDOWS_UWP
+using UnityEngine.Serialization;
+
+#if WINDOWS_UWP && MROPENXR_PRESENT
+using System;
 using Windows.Perception;
 using Windows.Perception.People;
 using Windows.Perception.Spatial;
@@ -15,7 +16,7 @@ using Windows.UI.Input.Spatial;
 namespace MixedReality.Toolkit.Input
 {
     /// <summary>
-    /// A helper class used to check eye calibration status. Only for UWP Platforms. 
+    /// A helper class used to check eye calibration status. Only for UWP Platforms.
     /// </summary>
     [AddComponentMenu("MRTK/Input/Eye Calibration Checker")]
     public class EyeCalibrationChecker : MonoBehaviour
@@ -23,27 +24,25 @@ namespace MixedReality.Toolkit.Input
         #region Serialized Fields
 
         /// <summary>
-        /// For testing purposes, you can manually assign whether eyes are calibrated or not in editor. 
+        /// For testing purposes, you can manually assign whether eyes are calibrated or not in editor.
         /// </summary>
-        [field: SerializeField, Tooltip("For testing purposes, you can manually assign whether eyes are calibrated or not in editor.")]
-        public EyeCalibrationStatus EditorTestIsCalibrated = EyeCalibrationStatus.Calibrated;
+        [field: SerializeField, FormerlySerializedAs("EditorTestIsCalibrated"), Tooltip("For testing purposes, you can manually assign whether eyes are calibrated or not in editor.")]
+        public EyeCalibrationStatus EditorTestIsCalibrated { get; set; } = EyeCalibrationStatus.Calibrated;
 
         #endregion Serialized Fields
 
         #region Private Fields
 
-        private EyeCalibrationStatus calibrationStatus;
-
         /// <summary>
-        /// Tracks whether eyes are present and calibrated. 
+        /// Tracks whether eyes are present and calibrated.
         /// </summary>
-        public EyeCalibrationStatus CalibratedStatus
-        {
-            get => calibrationStatus;
-        }
+        public EyeCalibrationStatus CalibratedStatus { get; private set; }
 
         private EyeCalibrationStatus prevCalibrationStatus;
-        private const int maxPoseAgeInSeconds = 1;
+
+#if WINDOWS_UWP && MROPENXR_PRESENT
+        private const int MaxPoseAgeInSeconds = 1;
+#endif
 
         #endregion Private Fields
 
@@ -79,7 +78,7 @@ namespace MixedReality.Toolkit.Input
         #endregion Events
 
         #region MonoBehaviour Functions
-        
+
         /// <summary>
         /// A Unity event function that is called every frame, if this object is enabled.
         /// </summary>
@@ -87,34 +86,35 @@ namespace MixedReality.Toolkit.Input
         {
             if (Application.isEditor)
             {
-                calibrationStatus = EditorTestIsCalibrated;
+                CalibratedStatus = EditorTestIsCalibrated;
             }
             else
             {
-                calibrationStatus = CheckCalibrationStatus();
+                CalibratedStatus = CheckCalibrationStatus();
             }
 
-            if (prevCalibrationStatus != calibrationStatus)
+            if (prevCalibrationStatus != CalibratedStatus)
             {
-                if (calibrationStatus == EyeCalibrationStatus.Calibrated)
+                if (CalibratedStatus == EyeCalibrationStatus.Calibrated)
                 {
                     calibrated.Invoke();
                 }
-                else if (calibrationStatus == EyeCalibrationStatus.NotCalibrated)
+                else if (CalibratedStatus == EyeCalibrationStatus.NotCalibrated)
                 {
                     notCalibrated.Invoke();
                 }
-                calibratedStatusChanged.Invoke(new EyeCalibrationStatusEventArgs(calibrationStatus));
-                prevCalibrationStatus = calibrationStatus;
+                calibratedStatusChanged.Invoke(new EyeCalibrationStatusEventArgs(CalibratedStatus));
+                prevCalibrationStatus = CalibratedStatus;
             }
         }
+
         #endregion MonoBehaviour Functions
 
         #region Private Functions
 
         private EyeCalibrationStatus CheckCalibrationStatus()
         {
-#if WINDOWS_UWP
+#if WINDOWS_UWP && MROPENXR_PRESENT
             if (Microsoft.MixedReality.OpenXR.PerceptionInterop.GetSceneCoordinateSystem(Pose.identity) is SpatialCoordinateSystem worldOrigin)
             {
                 SpatialPointerPose pointerPose = SpatialPointerPose.TryGetAtTimestamp(worldOrigin, PerceptionTimestampHelper.FromHistoricalTargetTime(DateTimeOffset.Now));
@@ -124,7 +124,7 @@ namespace MixedReality.Toolkit.Input
                     if (eyes != null)
                     {
                         // If it's been longer than a second since the last perception snapshot, assume the information has expired.
-                        if ((DateTimeOffset.Now - eyes.UpdateTimestamp.TargetTime).TotalSeconds > maxPoseAgeInSeconds)
+                        if ((DateTimeOffset.Now - eyes.UpdateTimestamp.TargetTime).TotalSeconds > MaxPoseAgeInSeconds)
                         {
                             return EyeCalibrationStatus.NotCalibrated;
                         }
@@ -142,7 +142,7 @@ namespace MixedReality.Toolkit.Input
             return EyeCalibrationStatus.NotTracked;
 #else
             return EyeCalibrationStatus.Unsupported;
-#endif // WINDOWS_UWP
+#endif // WINDOWS_UWP && MROPENXR_PRESENT
         }
 
         #endregion Private Functions
--- org.mixedrealitytoolkit.input/Utilities/EyeCalibration/EyeCalibrationStatus.cs
+++ org.mixedrealitytoolkit.input/Utilities/EyeCalibration/EyeCalibrationStatus.cs
@@ -3,35 +3,35 @@
 
 namespace MixedReality.Toolkit.Input
 {
-	/// <summary>
-	/// Used to track the current eye calibration status. 
-	/// </summary>
-	public enum EyeCalibrationStatus
-	{
-		/// <summary>
-		/// The eye calibration status is not defined. 
-		/// </summary>
-		Undefined,
+    /// <summary>
+    /// Used to track the current eye calibration status.
+    /// </summary>
+    public enum EyeCalibrationStatus
+    {
+        /// <summary>
+        /// The eye calibration status is not defined.
+        /// </summary>
+        Undefined,
 
-		/// <summary>
-		/// The eye calibration status could not be retrieved because this is an unsupported device.
-		/// </summary>
-		Unsupported,
+        /// <summary>
+        /// The eye calibration status could not be retrieved because this is an unsupported device.
+        /// </summary>
+        Unsupported,
 
-		/// <summary>
-		/// The eye calibration status could not be retrieved because eyes are not being tracked.
-		/// This usually occurs when SpatialPointerPose's Eyes property is null. 
-		/// </summary>
-		NotTracked,
+        /// <summary>
+        /// The eye calibration status could not be retrieved because eyes are not being tracked.
+        /// This usually occurs when SpatialPointerPose's Eyes property is null.
+        /// </summary>
+        NotTracked,
 
-		/// <summary>
-		/// The eye calibration status was retrieved and eyes are not calibrated. 
-		/// </summary> 
-		NotCalibrated,
+        /// <summary>
+        /// The eye calibration status was retrieved and eyes are not calibrated.
+        /// </summary>
+        NotCalibrated,
 
-		/// <summary>
-		/// The eye calibration status was retrieved and eyes are calibrated. 
-		/// </summary>
-		Calibrated
-	};
+        /// <summary>
+        /// The eye calibration status was retrieved and eyes are calibrated.
+        /// </summary>
+        Calibrated
+    }
 }
